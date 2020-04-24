89f23c66f6a83cd954db0d00d09fd4bcc3ae2258
Phua June Jin <jin.phua@mygomana.com>
2020-04-24 19:21:32 +0800

    FEATURE: Draw 2 x 2 or 3 x 3 square inside the canvas of 400 x 400

--- Assessment/ViewController.h
+++ Assessment/ViewController.h
@@ -8,8 +8,23 @@
 
 #import <UIKit/UIKit.h>
 
+#pragma mark - Enum (Options)
+typedef NS_ENUM(NSInteger, GraphicOption) {
+    GRAPHIC_OPTION_DRAW_2_X_2_SQUARE,
+    GRAPHIC_OPTION_DRAW_3_X_3_SQUARE,
+    GRAPHIC_OPTION_DRAW_APPLY_FILTER,
+};
+
+#pragma mark - Constant
+static NSInteger const CANVAS_HEIGHT = 400;
+static NSInteger const CANVAS_WIDTH  = 400;
+static NSInteger const MARGIN        =  20;
+static NSInteger const LINE_WIDTH    =   4;
+
+#pragma mark - Interface (View Controller)
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIView *canvas;
 
 @end
 
--- Assessment/ViewController.m
+++ Assessment/ViewController.m
@@ -14,10 +14,94 @@
 
 @implementation ViewController
 
+#pragma mark - Lifecycle
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+
+#pragma mark - IBAction
+- (IBAction)buttonPressed:(id)sender {
+    UIButton *button = (UIButton *) sender;
+
+    GraphicOption tag = button.tag;
+
+    switch (tag) {
+        case GRAPHIC_OPTION_DRAW_2_X_2_SQUARE:
+            [self drawSquares:2];
+            break;
+        case GRAPHIC_OPTION_DRAW_3_X_3_SQUARE:
+            [self drawSquares:3];
+            break;
+        case GRAPHIC_OPTION_DRAW_APPLY_FILTER:
+            [self applyFilter];
+            break;
+        default:
+            break;
+    }
+}
+
+#pragma mark - Methods (Draw)
+- (void)drawSquares:(NSInteger)n {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSInteger length = (CANVAS_HEIGHT - (MARGIN + LINE_WIDTH) * (n + 1)) / n;
+
+        NSMutableArray *array = [NSMutableArray new];
+
+        dispatch_group_t group = dispatch_group_create();
+
+        dispatch_queue_t serial = dispatch_queue_create("com.123RF.Assessment", DISPATCH_QUEUE_SERIAL);
+
+        for (int i = 0 ; i < n ; i++) {
+            for (int j = 0 ; j < n ; j++) {
+                dispatch_group_async(group, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                    CAShapeLayer *square = [self generateSquare:length PositionX:MARGIN + (MARGIN + LINE_WIDTH + length) * i PositionY:MARGIN + (MARGIN + LINE_WIDTH + length) * j];
+
+                    dispatch_async(serial, ^{
+                        [array addObject:square];
+                    });
+                });
+            }
+        }
+
+        dispatch_group_notify(group, serial, ^{
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.canvas.layer.sublayers = nil;
+
+                for (CAShapeLayer *layer in array) {
+                    [self.canvas.layer addSublayer:layer];
+                }
+            });
+        });
+    });
+}
+
+#pragma mark - Methods (Apply Filter)
+- (void)applyFilter {
+    //TODO: Apply Filter
+}
+
+#pragma mark - Methods (Helper)
+- (CAShapeLayer *)generateSquare:(NSInteger)length PositionX:(NSInteger)positionX PositionY:(NSInteger)positionY {
+    CAShapeLayer *square = [CAShapeLayer layer];
+
+    square.lineWidth = LINE_WIDTH;
+    square.fillColor = [self generateRandomColor];
+    square.strokeColor = UIColor.blackColor.CGColor;
+    square.path = [UIBezierPath bezierPathWithRect:CGRectMake(positionX, positionY, length, length)].CGPath;
+
+    return square;
+}
+
+- (CGColorRef)generateRandomColor {
+    CGFloat redValue = (CGFloat) arc4random() / UINT32_MAX;
+    CGFloat greenValue = (CGFloat) arc4random() / UINT32_MAX;
+    CGFloat blueValue = (CGFloat) arc4random() / UINT32_MAX;
+    CGFloat alphaValue = (CGFloat) arc4random() / UINT32_MAX;
+
+    return [UIColor colorWithRed:redValue green:greenValue blue:blueValue alpha:alphaValue].CGColor;
+}
+
 
 
 @end
