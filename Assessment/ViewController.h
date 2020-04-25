58dafffa4a3b79c3258ff43702a5afc4d766e1f4
Phua June Jin <jin.phua@mygomana.com>
2020-04-25 12:33:24 +0800

    FEATURE: Load Image to canvas itself or one of the square inside the canvas 	 Apply Vibrance filter and CLAHE filter to the image, and reload the image into the canvas or square

--- Assessment/ViewController.h
+++ Assessment/ViewController.h
@@ -7,24 +7,34 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MetalPetal/MetalPetal.h>
 
 #pragma mark - Enum (Options)
 typedef NS_ENUM(NSInteger, GraphicOption) {
     GRAPHIC_OPTION_DRAW_2_X_2_SQUARE,
     GRAPHIC_OPTION_DRAW_3_X_3_SQUARE,
     GRAPHIC_OPTION_DRAW_APPLY_FILTER,
+    GRAPHIC_OPTION_CLEAR_CANVAS,
+    GRAPHIC_OPTION_LOAD_IMAGE
 };
 
 #pragma mark - Constant
-static NSInteger const CANVAS_HEIGHT = 400;
-static NSInteger const CANVAS_WIDTH  = 400;
-static NSInteger const MARGIN        =  20;
-static NSInteger const LINE_WIDTH    =   4;
+static NSInteger const CANVAS_HEIGHT          = 400;
+static NSInteger const CANVAS_WIDTH           = 400;
+static NSInteger const MARGIN                 =  20;
+static NSInteger const LINE_WIDTH             =   4;
+static NSInteger const VIBRANCE_FILTER_AMOUNT =   2;
 
 #pragma mark - Interface (View Controller)
 @interface ViewController : UIViewController
 
+@property (strong, nonatomic) MTIContext *context;
+
 @property (weak, nonatomic) IBOutlet UIView *canvas;
+@property (weak, nonatomic) IBOutlet UIButton *button2x2;
+@property (weak, nonatomic) IBOutlet UIButton *button3x3;
+@property (weak, nonatomic) IBOutlet UIButton *buttonApplyFilter;
+@property (weak, nonatomic) IBOutlet UIButton *buttonLoadImage;
+@property (weak, nonatomic) IBOutlet UIButton *buttonClearCanvas;
 
 @end
-
--- Assessment/ViewController.m
+++ Assessment/ViewController.m
@@ -8,16 +8,25 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
-@end
-
 @implementation ViewController
 
 #pragma mark - Lifecycle
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    self.context = [[MTIContext alloc] initWithDevice:MTLCreateSystemDefaultDevice() error:nil];
+
+    self.button2x2.tintColor = UIColor.labelColor;
+    self.button3x3.tintColor = UIColor.labelColor;
+    self.buttonApplyFilter.tintColor = UIColor.labelColor;
+    self.buttonClearCanvas.tintColor = UIColor.labelColor;
+    self.buttonLoadImage.tintColor = UIColor.labelColor;
+
+    self.button2x2.backgroundColor = UIColor.systemGray6Color;
+    self.button3x3.backgroundColor = UIColor.systemGray6Color;
+    self.buttonApplyFilter.backgroundColor = UIColor.systemGray6Color;
+    self.buttonClearCanvas.backgroundColor = UIColor.systemGray6Color;
+    self.buttonLoadImage.backgroundColor = UIColor.systemGray6Color;
 }
 
 #pragma mark - IBAction
@@ -35,6 +44,12 @@
             break;
         case GRAPHIC_OPTION_DRAW_APPLY_FILTER:
             [self applyFilter];
+            break;
+        case GRAPHIC_OPTION_CLEAR_CANVAS:
+            [self clearCanvas];
+            break;
+        case GRAPHIC_OPTION_LOAD_IMAGE:
+            [self loadImage:[UIImage imageNamed:@"image.jpg"] Index:-1];
             break;
         default:
             break;
@@ -55,7 +70,7 @@
         for (int i = 0 ; i < n ; i++) {
             for (int j = 0 ; j < n ; j++) {
                 dispatch_group_async(group, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                    CAShapeLayer *square = [self generateSquare:length PositionX:MARGIN + (MARGIN + LINE_WIDTH + length) * i PositionY:MARGIN + (MARGIN + LINE_WIDTH + length) * j];
+                    CALayer *square = [self generateSquare:length PositionX:MARGIN + (MARGIN + LINE_WIDTH + length) * i PositionY:MARGIN + (MARGIN + LINE_WIDTH + length) * j];
 
                     dispatch_async(serial, ^{
                         [array addObject:square];
@@ -66,9 +81,10 @@
 
         dispatch_group_notify(group, serial, ^{
             dispatch_async(dispatch_get_main_queue(), ^{
+                self.canvas.layer.contents = nil;
                 self.canvas.layer.sublayers = nil;
 
-                for (CAShapeLayer *layer in array) {
+                for (CALayer *layer in array) {
                     [self.canvas.layer addSublayer:layer];
                 }
             });
@@ -76,32 +92,89 @@
     });
 }
 
+#pragma mark - Methods (Load Image into CALayer)
+- (void)loadImage:(UIImage *)image Index:(NSInteger)index {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSArray *array = self.canvas.layer.sublayers;
+
+        if (array.count) {
+            for (CALayer *layer in array) {
+                layer.contents = nil;
+            }
+
+            CALayer *layer = index >= 0 ? array[index] : array[arc4random() % array.count];
+
+            layer.contents = (id) image.CGImage;
+        } else {
+            self.canvas.layer.contents = (id) image.CGImage;
+        }
+    });
+}
+
+- (void)clearCanvas {
+    self.canvas.layer.contents = nil;
+    self.canvas.layer.sublayers = nil;
+}
+
 #pragma mark - Methods (Apply Filter)
 - (void)applyFilter {
-    //TODO: Apply Filter
+    BOOL hasImageInCanvas = self.canvas.layer.contents;
+
+    NSArray *array = self.canvas.layer.sublayers;
+
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        if (array.count) {
+            for (int i = 0 ; i < array.count ; i++) {
+                CALayer *layer = array[i];
+
+                if (layer.contents) {
+                    [self loadImage:[self applyFilterToImage] Index:i];
+
+                    break;
+                }
+            }
+        } else if (hasImageInCanvas) {
+            [self loadImage:[self applyFilterToImage] Index:0];
+        }
+    });
 }
 
 #pragma mark - Methods (Helper)
-- (CAShapeLayer *)generateSquare:(NSInteger)length PositionX:(NSInteger)positionX PositionY:(NSInteger)positionY {
-    CAShapeLayer *square = [CAShapeLayer layer];
+- (CALayer *)generateSquare:(NSInteger)length PositionX:(NSInteger)positionX PositionY:(NSInteger)positionY {
+    CALayer *layer = [CALayer layer];
 
-    square.lineWidth = LINE_WIDTH;
-    square.fillColor = [self generateRandomColor];
-    square.strokeColor = UIColor.blackColor.CGColor;
-    square.path = [UIBezierPath bezierPathWithRect:CGRectMake(positionX, positionY, length, length)].CGPath;
+    layer.frame = CGRectMake(positionX, positionY, length, length);
+    layer.borderWidth = LINE_WIDTH;
+    layer.borderColor = UIColor.labelColor.CGColor;
 
-    return square;
+    return layer;
 }
 
-- (CGColorRef)generateRandomColor {
-    CGFloat redValue = (CGFloat) arc4random() / UINT32_MAX;
-    CGFloat greenValue = (CGFloat) arc4random() / UINT32_MAX;
-    CGFloat blueValue = (CGFloat) arc4random() / UINT32_MAX;
-    CGFloat alphaValue = (CGFloat) arc4random() / UINT32_MAX;
+- (UIImage *)applyFilterToImage {
+    MTIImage *image = [[MTIImage alloc] initWithCGImage:[UIImage imageNamed:@"image.jpg"].CGImage options:nil];
 
-    return [UIColor colorWithRed:redValue green:greenValue blue:blueValue alpha:alphaValue].CGColor;
+    image = [image imageWithCachePolicy:MTIImageCachePolicyTransient];
+
+    MTIVibranceFilter *vibranceFilter = [MTIVibranceFilter new];
+
+    vibranceFilter.amount = VIBRANCE_FILTER_AMOUNT;
+    vibranceFilter.inputImage = image;
+
+    MTIImage *postVirbanceFilterImage = vibranceFilter.outputImage;
+
+    MTICLAHEFilter *claheFilter = [MTICLAHEFilter new];
+
+    claheFilter.inputImage = postVirbanceFilterImage;
+
+    MTIImage *postClaheFilterImage = claheFilter.outputImage;
+
+    CGImageRef ref = [self.context createCGImageFromImage:postClaheFilterImage error:nil];
+
+    UIImage *postProcessedImage = [[UIImage alloc] initWithCGImage:ref];
+
+    CFRelease(ref);
+
+    return postProcessedImage;
 }
-
-
 
 @end
